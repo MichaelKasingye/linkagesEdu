0838acc8397f462302dddcf54373dcedf2c5071a
HellenNamulinda <hellennamulinda@gmail.com>
2021-08-18 10:03:48 +0300

    CHORE: Update project structure

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-     <h1>Home</h1>
-<button>  <Link href='/course'>course</Link> </button>
+     <h1>All Opportunities</h1>
+<button>  <Link href='/profile'>create profile</Link> </button>
     </div>
   )
 }
