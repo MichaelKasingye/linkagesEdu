35a1ef53181c3f4577428b9197b376cb8847d920
MichaelKasingye <michaelkasingye@gmail.com>
2021-08-19 12:14:33 +0300

    FEAT:add left and right sections

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import LeftSide from '../components/Left/LeftBody/LeftSide';
 import RightSide from '../components/Right/RightBody/RightSide';
 import general from '../styles/general.module.css'
+import Link from "next/link";
+
 function course() {
   return (
     <div className = {general.wrapper}>
       <LeftSide/>
       <RightSide/>
+      <h1>All Opportunities</h1>
+<button>  <Link href='/profile'>create profile</Link> </button>
     </div>
   )
 }
