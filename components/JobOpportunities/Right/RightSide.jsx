3cd95d345ae6be54e22cfe25046369380a8432e6
HellenNamulinda <hellennamulinda@gmail.com>
2021-08-20 10:23:52 +0300

    CHORE: Structuring pages

---
+++ components/JobOpportunities/Right/RightSide.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TitleRight from '../../TitleRight/TitleRight';
+import rightCss from '../../../styles/body/RightBody.module.css';
+
+function RightSide() {
+    return (
+        <section className={rightCss.right}>
+
+       <TitleRight title= "Job Opportunities"/>
+        </section>
+    )
+}
+
+export default RightSide
