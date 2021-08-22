4eaf81844a2126e039b9b6bcc474087c58a5bd13
Scovia Achan <achanscovia@gmail.com>
2021-08-22 21:18:04 +0300

    CHORE: modify opportunities page

--- pages/allOpportunities.jsx
+++ pages/allOpportunities.jsx
@@ -1,28 +1,10 @@
 import opportStyles from "../styles/body/AllOpportunities.module.css";
 import OpportunityCard from "../components/Opportunity/OpportunityCard";
 import SearchBar from "../components/SearchBar/SearchBar";
+import RightSide from "../components/JobOpportunities/Right/RightSide";
 
 function Opportunities() {
-  return (
-    <div className={opportStyles.allApportunities}>
-      <h2>Job Opportunities</h2>
-      <SearchBar />
-      <div className={opportStyles.flexitem}>
-        {opport.map(
-          ({ key, jobTitle, company, location, paragraph, deadline }) => (
-            <OpportunityCard
-              key={key}
-              jobTitle={jobTitle}
-              company={company}
-              location={location}
-              paragraph={paragraph}
-              deadline={deadline}
-            />
-          )
-        )}
-      </div>
-    </div>
-  );
+  return <div className={opportStyles.allApportunities}></div>;
 }
 
 export default Opportunities;
